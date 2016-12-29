22b2f45440ebd83b42a5af0ec55f4fc37c592d45
Kabul Dasa Riyatmaja - 1406623745 <kabul.dasa@badak.cs.ui.ac.id>
2016-12-29 14:24:38 +0700

    UAS-REM revision

--- XREMEDY2/ambil.sh
+++ XREMEDY2/ambil.sh
@@ -1,15 +1,18 @@
 # (c) 2016 Kabul Dasa R -- This is free software
-# REV00 Thu Dec 29 12:27:14 WIB 2016
+# REV01 Thu Dec 29 14:14:21 WIB 2016 - remove bracket
+# REV00 Thu Dec 29 12:27:14 WIB 2016 - extract content
 # START Thu Dec 29 09:20:34 WIB 2016
-# script menyimpan webpage dalam format text dan mengambil konten
-# yang berada diantara string ZCZC dan NNNN
+# script menyimpan webpage dalam format text, mengambil konten
+# yang berada diantara string ZCZC dan NNNN, dan menghapus bracket'[]'
 
-start="================================================== START BERKAS"
-stop="================================================== STOP BERKAS"
+start="========================================================== START BERKAS"
+stop="========================================================== STOP BERKAS"
 page="$(w3m -dump http://os162.vlsm.org/2016/11/ranking-os162.html)"
 
 echo "$page" 					> temp
-sed -n '/ZCZC/,/NNNN/{/ZCZC/b;/NNNN/b;p}' temp	> rangking.txt
+sed -n '/ZCZC/,/NNNN/{/ZCZC/b;/NNNN/b;p}' temp	> temp1
+echo "$start$(cat temp1)"			> temp1
+echo "$stop"				       >> temp1
+sed 's/\[//g;s/\]//g' temp1			> rangking.txt
 rm temp
-echo "$start$(cat rangking.txt)"		> rangking.txt
-echo "$stop"				       >> rangking.txt
+rm temp1
